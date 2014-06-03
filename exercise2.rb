c72ffd5ea06ac6d6a7dd5ccfc382374c1ffd1741
Josh Cornelius <joshcornelius9@gmail.com>
2014-06-03 12:11:49 -0400

    added exercise3

---
+++ exercise2.rb
@@ -0,0 +1,21 @@
+#question1
+
+meal_cost = 55
+tip = meal_cost * 0.15
+
+puts tip
+
+#question2
+
+str = "this is a string + "
+int = 5
+
+puts str + int.to_s
+
+#question3
+
+puts "Outputting the result of some multiplication: #{45628 * 7839}"
+
+#question4
+
+puts (true && false) || (false && true) || !(false && false)
